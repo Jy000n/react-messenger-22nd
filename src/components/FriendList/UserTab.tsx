f66efe6150237112de4b703982f60ac928edacbc
jy000n <jayoonchang@naver.com>
2025-09-29 16:38:16 +0900

    feat: 친구목록 유저프로필 UI

--- src/components/FriendList/UserTab.tsx
+++ src/components/FriendList/UserTab.tsx
@@ -1,23 +1,13 @@
+import ProflieIMGDefault from '@/assets/svgs/profile/profileIMG-default.svg';
+
 const UserTab = () => {
   return (
-    <>
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-      유저탭
-      <br />
-    </>
+    <div className="w-full cursor-pointer p-[20px]">
+      <div className="flex flex-row rounded-[4px] bg-[#815840] p-[10px]">
+        <img src={ProflieIMGDefault} alt="유저프사" className="h-[54px] w-[54px]" />
+        <span className="ml-[15px] flex items-center text-[16px] font-semibold text-white">사용자</span>
+      </div>
+    </div>
   );
 };
 
